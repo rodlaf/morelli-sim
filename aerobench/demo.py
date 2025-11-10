793307ef24f9dc66f5c14ec3f0abb497c058bded
Rodney Lafuente Mercado <rodneyslafuente@gmail.com>
2025-11-09 23:12:06 -0600

    remove util.py

--- aerobench/demo.py
+++ aerobench/demo.py
@@ -11,8 +11,7 @@ from math import pi, atan2, sqrt, sin, cos, asin
 import numpy as np
 from numpy import deg2rad, rad2deg
 
-from aerobench.f16_waypoint import F16Waypoint
-from aerobench.util import StateIndex
+from aerobench.f16_waypoint import F16Waypoint, StateIndex
 
 
 class AutopilotAgent:
--- aerobench/f16_waypoint.py
+++ aerobench/f16_waypoint.py
@@ -24,8 +24,84 @@ import numpy as np
 from typing import Tuple, Dict, Any
 
 from aerobench import f16_model
-from aerobench.util import get_state_names, Euler, StateIndex
 from aerobench import raylib_renderer_cy as raylib_renderer
+
+
+class StateIndex:
+    """State variable indices"""
+    VT = 0
+    VEL = 0  # alias
+    ALPHA = 1
+    BETA = 2
+    PHI = 3  # roll angle
+    THETA = 4  # pitch angle
+    PSI = 5  # yaw angle
+    P = 6
+    Q = 7
+    R = 8
+    POSN = 9
+    POS_N = 9
+    POSE = 10
+    POS_E = 10
+    ALT = 11
+    H = 11
+    POW = 12
+
+
+class Euler:
+    """Fixed step Euler integration"""
+    
+    def __init__(self, der_func, tstart, ystart, tend, step=0, time_tol=1e-9):
+        assert step > 0, "arg step > 0 required in Euler integrator"
+        assert tend > tstart
+        
+        self.der_func = der_func
+        self.tstep = step
+        self.t = tstart
+        self.y = ystart.copy()
+        self.yprev = None
+        self.tprev = None
+        self.tend = tend
+        self.status = 'running'
+        self.time_tol = time_tol
+    
+    def step(self):
+        """Take one integration step"""
+        if self.status == 'running':
+            self.yprev = self.y.copy()
+            self.tprev = self.t
+            yd = self.der_func(self.t, self.y)
+            
+            self.t += self.tstep
+            
+            if self.t + self.time_tol >= self.tend:
+                self.t = self.tend
+            
+            dt = self.t - self.tprev
+            self.y += dt * yd
+            
+            if self.t == self.tend:
+                self.status = 'finished'
+    
+    def dense_output(self):
+        """Return a function for linear interpolation between steps"""
+        assert self.tprev is not None
+        
+        dy = self.y - self.yprev
+        dt = self.t - self.tprev
+        dydt = dy / dt
+        
+        def fun(t):
+            """Return state at time t (linear interpolation)"""
+            deltat = t - self.tprev
+            return self.yprev + dydt * deltat
+        
+        return fun
+
+
+def get_state_names():
+    """Returns list of state variable names"""
+    return ['vt', 'alpha', 'beta', 'phi', 'theta', 'psi', 'P', 'Q', 'R', 'pos_n', 'pos_e', 'alt', 'pow']
 
 
 class F16Waypoint:
